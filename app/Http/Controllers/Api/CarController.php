59148a218db3c9239ec522d6ab16260570edc5b0
codepundit1 <jahid35-266@diu.edu.bd>
2022-12-27 21:17:39 +0600

    data create complete through api

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -12,13 +12,39 @@ class CarController extends Controller
 
     public function index()
     {
-        $cars = Car::latest()->get();
+        $cars = Car::latest()->paginate(2);
         return CarResource::collection($cars);
     }
 
 
     public function store(Request $request)
     {
+        $valid = $request->validate([
+            'title' => ['required', 'string', 'max:255'],
+            'driver_id' => ['required', 'integer'],
+            'car_number' => ['required', 'integer', 'digits_between:5,20', 'unique:cars'],
+            'model' => ['nullable', 'string', 'max:9999'],
+            'price' => ['nullable', 'numeric', 'gt:0'],
+            'cover_image' => ['nullable', 'image', 'max:2048'],
+        ]);
+
+        if ($request->hasFile('cover_image'))
+        {
+            try
+            {
+                $valid['cover_image'] = $request->file('cover_image')->store('CarImages', 's3');
+            }
+            catch (\Exception $exception)
+            {
+                return back()->with('error', $exception->getMessage());
+            }
+        }
+
+        $car = Car::create($valid);
+
+        if($car)
+            return response()->json(['message' => 'Car successfully created', new CarResource($car)]);
+        return response()->json(['error' => 'Something went wrong'], 500);
     }
 
 
